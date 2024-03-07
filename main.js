34ee2fdfe7e45d1e3e22e43d226787ed31a9a9c7
SdZika <markozivkovic1982@gmail.com>
2024-03-07 11:43:02 +0100

    add values to main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+const firstNumber = 13; // type of value number
+let secondNumber = 14; // type of value number
+
+const firstString = "Marko"; // type of value string
+let seconString = "Zivkovic"; // type of value string
+
+typeof firstUndefind; // type of value undefind
+
+// boolean type of values true
+console.log(3 > 2); //  true
+console.log(3 < 2); // false
+console.log(true && false); // logical operation false
+console.log(true && true); //  logical operation true
+
+const firstArithmetic = 5 + 5;
+console.log(firstArithmetic);
+let secondArithmetic = 3 * 3; 
+console.log(secondArithmetic); // arithmetic
+
+
+
+
+
+
